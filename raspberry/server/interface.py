9532e69e8d1cc99d756016d1f6aa5786cae4fcc6
kuh0729 <41353984+kuh0729@users.noreply.github.com>
2018-12-07 16:19:36 +0100

    Add files via upload

--- raspberry/server/interface.py
+++ raspberry/server/interface.py
@@ -1,16 +1,16 @@
 from data import Data
 from data import ID
 from data import Message
-from threading import Thread
+import threading
 import socket
 
-HOST = ''                 # Symbolic name meaning all available interfaces
-PORT = 6666              # Arbitrary non-privileged port
+HOST = ''  # Symbolic name meaning all available interfaces
+PORT = 6666  # Arbitrary non-privileged port
+
+global DATAINTERFACE  # global variable.
 
 
-global DATAINTERFACE #global variable. 
-
-class Interface(Thread): 
+class Interface(threading.Thread):
     def __init__(self):
         Thread.__init__(self)
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -18,41 +18,41 @@ class Interface(Thread):
         s.listen(1)
         self.conn, addr = s.accept()
         print('Connected by', addr)
-        
+
     def run(self):
 
-        while True :
+        while True:
             data = self.conn.recv(1024)
 
-            if not data : break
-                
+            if not data: break
+
             header = data[0:3]
             payload = data[3:]
             print("header :", header, "payload:", str(payload))
-            
-            if (header == b'STE'): # steer
+
+            if (header == b'STE'):  # steer
                 if (payload == b'left'):
-                    DATAINTERFACE = Data(ID.INTERFACE,Message.LEFT)
+                    DATAINTERFACE = Data(ID.INTERFACE, Message.LEFT)
                     print(DATAINTERFACE.message.value)
                 elif (payload == b'right'):
-                    DATAINTERFACE = Data(ID.INTERFACE,Message.RIGHT)
+                    DATAINTERFACE = Data(ID.INTERFACE, Message.RIGHT)
                     print(DATAINTERFACE.message.value)
             elif (header == b'MOV'):  # move
                 if (payload == b'stop'):
-                    DATAINTERFACE = Data(ID.INTERFACE,Message.STOP)
+                    DATAINTERFACE = Data(ID.INTERFACE, Message.STOP)
                     print(DATAINTERFACE.message.value)
                 elif (payload == b'forward'):
-                    DATAINTERFACE = Data(ID.INTERFACE,Message.FORWARD)
+                    DATAINTERFACE = Data(ID.INTERFACE, Message.FORWARD)
                     print(DATAINTERFACE.message.value)
                 elif (payload == b'backward'):
-                    DATAINTERFACE = Data(ID.INTERFACE,Message.BACKWARD)
+                    DATAINTERFACE = Data(ID.INTERFACE, Message.BACKWARD)
                     print(DATAINTERFACE.message.value)
-            elif (header == b'AUT'): #autonomous mode
-                DATAINTERFACE = Data(ID.INTERFACE,Message.AUTONOMOUS)
+            elif (header == b'AUT'):  # autonomous mode
+                DATAINTERFACE = Data(ID.INTERFACE, Message.AUTONOMOUS)
                 print(DATAINTERFACE.message.value)
-            
-        conn.close()   
-        
+
+        conn.close()
+
 
 '''
 inter = Interface()
@@ -61,17 +61,16 @@ inter.run()
 
 
 
-        
-        
 
 
 
-    
-            
-            
-            
-            
-            
-            
-            
-            
+
+
+
+
+
+
+
+
+
+
