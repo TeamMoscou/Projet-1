0f717380ba8f381fef319ed53df1deaf5a527569
TeamMoscou_INSA_TLSE_GEI <43847983+TeamMoscou@users.noreply.github.com>
2018-12-07 17:52:50 +0100

    Add files via upload

--- raspberry/server/prise_de_decision.py
+++ raspberry/server/prise_de_decision.py
@@ -1,4 +1,4 @@
-#from threading import Thread
+from threading import Thread
 import threading
 import data
 from data import Data
@@ -9,7 +9,7 @@ class ID(Enum):
     LIDAR = 1
     ULTRASONIC = 2
     INTERFACE = 3
-    AUTRE = 4
+    DECISION = 4
 
 class Message(Enum):
     FORWARD = 1
@@ -18,10 +18,14 @@ class Message(Enum):
     RIGHT = 4
     STOP = 5
     AUTONOMOUS = 6
-    DETECTED_AVANT = 7
-    DETECTED_ARRIERE = 8
+    DETECTED_FRONT = 7
+    DETECTED_BACk = 8
+    FORWARD_LEFT = 9
+    FORWARD_RIGHT = 10
+    BACKWARD_LEFT = 11
+    BACKWARD_RIGHT = 12
 '''
-class Prise_decision(threading.Thread):
+class Prise_decision(Thread):
 
     def prise_decision():
         global Mode
@@ -29,24 +33,84 @@ class Prise_decision(threading.Thread):
         global DataUltrason
         global DataInterface
         global DataOut # les noms sont à voir
-        if (Mode=="Pilote" or Mode=="Autonomous"):
-            if (DataInterface.message.value == 5 or ((DataLidar.message.value == 7 or DataUltrason.message.value == 7) and DataInterface.message.value==1) or ((DataLidar.message.value == 8 or DataUltrason.message.value == 8 )and DataInterface.message.value == 2 )):
-                DataOut.message=Message.STOP
-                if (Mode=="Pilote"):
-                    Mode="Autonomous"
-            else:
-                DataOut.message=DataInterface.message
+        Detection_front = 0
+        Detection_back = 0
+        Stop_requested = 0
+        Forward = 0
+        Backward = 0
 
-    '''
-    #Partie test
-    DataLidar = Data(ID.LIDAR,Message.DETECTED_ARRIERE)
-    DataUltrason = Data(ID.ULTRASONIC,Message.DETECTED_AVANT)
-    DataInterface = Data(ID.INTERFACE,Message.FORWARD)
-    DataOut = Data(ID.AUTRE,Message.FORWARD )
+        #Recuperation donnees
+        if (DataInterface.message == Message.STOP):
+        
+            Stop_requested = 1
+
+        if (DataUltrason.message == Message.DETECTED_FRONT or DataLidar.message == Message.DETECTED_FRONT):
+
+            Detection_front = 1
+
+        if (DataUltrason.message == Message.DETECTED_BACK or DataLidar.message == Message.DETECTED_BACK):
+
+            Detection_back = 1
+
+        if (DataInterface.message == Message.FORWARD or DataInterface.message == Message.FORWARD_RIGHT or DataInterface.message == Message.FORWARD_LEFT):
+            
+            Forward = 1
+        
+        if (DataInterface.message == Message.BACKWARD or DataInterface.message == Message.BACKWARD_RIGHT or DataInterface.message == Message.BACKWARD_LEFT):
+
+            Backward = 1
+
     
-    Mode="Pilote"
-    prise_decision()
-    print(DataOut.message)
-    print(Mode)
-    '''
+        #Utilisation donnes
+        
+        #Si stop demande, on stop
+
+        if (Stop_requested):
+            
+            DataOut.message=Message.STOP
+            
+        #Si detection_avant et on avance, on stop
+        elif (Detection_front and Forward):
+            
+            DataOut.message=Message.STOP
+            
+            #Si mode pilote, on indique qu'on passe en autonome
+            if (Mode=="PILOTE"):
+                
+                Mode="AUTONOMOUS"
+                
+        #Si detection_arriere et on recule, on stop
+        elif (Detection_back and Backward):
+            
+            DataOut.message=message.STOP
+            
+            #Si mode pilote, on indique qu'on passe en autonome
+            if (Mode=="PILOTE"):
+                
+                Mode="AUTONOMOUS"
+                
+        #Si aucun des cas precedents, on transmets juste le message de l'interface
+        else:
+            DataOut.message=DataInterface.message
+        #print("detection avant:", Detection_front)
+        #print("detection arr:" , Detection_back)
+        #print("demande stop:" , Stop_requested)
+        #print("backward:" , Backward)
+        #print("forward:" , Forward)
+        
+
+'''
+#Partie test
+DataLidar = Data(ID.LIDAR,Message.DETECTED_FRONT)
+DataUltrason = Data(ID.ULTRASONIC,Message.DETECTED_FRONT)
+DataInterface = Data(ID.INTERFACE,Message.FORWARD)
+DataOut = Data(ID.DECISION,Message.FORWARD )
+
+Mode="PILOTE"
+prise_decision()
+print(DataOut.message)
+print(Mode)
+
+'''
+
 
