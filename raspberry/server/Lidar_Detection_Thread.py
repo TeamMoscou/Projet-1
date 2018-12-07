e91a5d44d59e91332882c20e7f94eb8121f3d2ff
TeamMoscou_INSA_TLSE_GEI <43847983+TeamMoscou@users.noreply.github.com>
2018-12-07 16:00:35 +0100

    Update Lidar_Detection_Thread.py

--- raspberry/server/Lidar_Detection_Thread.py
+++ raspberry/server/Lidar_Detection_Thread.py
@@ -10,7 +10,6 @@ import time
 
 
 lidar = RPLidar('/dev/ttyUSB0')
-
 SAFE_DISTANCE=2000
 ANGLE_MAX_FRONT=200
 ANGLE_MIN_FRONT=160
@@ -31,23 +30,19 @@ class LidarDetection(Thread):
 
         global Flag_FRONT
         global Flag_BACK
-        
         count_points=0
         count_points_detected_FRONT=0
         count_points_detected_BACK=0
-        
+
         new_scan=True
         angle=0.0
         quality=0
         distance=0.0
 
-
+        time.sleep(5)
         for new_scan, quality, angle, distance in lidar.iter_measurments() :
 
-            print(new_scan, quality, angle, distance)
-
             if(not(new_scan) and distance!=0) :
-
                 count_points=count_points+1
 
                 if (distance<=SAFE_DISTANCE and angle>=ANGLE_MIN_FRONT and angle<=ANGLE_MAX_FRONT) :
@@ -58,7 +53,7 @@ class LidarDetection(Thread):
             print("number of points detected in front",count_points_detected_FRONT,"\n")
             print("number of points detected in back",count_points_detected_BACK,"\n")
 
-            if(count_points=360):
+            if(count_points==320):
 
                count_points=0
 
@@ -73,4 +68,21 @@ class LidarDetection(Thread):
                else :
                   Flag_BACK=0
                count_points_detected_BACK=0
+            print("FLAG FRONT     ",Flag_FRONT,"\n")
+            print("FLAG  back     ",Flag_BACK,"\n")
 
+#il faut commenter avant de faire import Lidar_Detection_Thread
+#ça sert juste à tester le Thread tout seul
+
+if __name__ == "__main__":
+        try:
+               newDetect=LidarDetection()
+               newDetect.start()
+               newDetect.join()
+
+        except KeyboardInterrupt:
+               print('Stoping.')
+               lidar.stop()
+               lidar.stop_motor()
+               lidar.disconnect()
+               lidar.clear_input()
