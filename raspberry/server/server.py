1a400e913f67b39f77a4e05471f8785ebd10b30c
kuh0729 <41353984+kuh0729@users.noreply.github.com>
2018-12-07 16:41:59 +0100

    Add files via upload

--- raspberry/server/server.py
+++ raspberry/server/server.py
@@ -1,11 +1,15 @@
 import Lidar_Detection_Thread
 import interface
+import Ultrason
 
 lidar_instance = Lidar_Detection_Thread
 interface_instance = interface
+Ultrason_instance = Ultrason
 
 lidar_thread = lidar_instance.LidarDetection()
 interface_thread = interface_instance.Interface()
+ultrason_thread = Ultrason_instance.Ultrason()
 
 lidar_thread.start()
 interface_thread.start()
+ultrason_thread.start()
