6f227110552a19c6c6d731acb07ab9f9f25a4046
kuh0729 <41353984+kuh0729@users.noreply.github.com>
2018-12-11 08:44:08 +0100

    Add files via upload

--- raspberry/server/server.py
+++ raspberry/server/server.py
@@ -1,6 +1,6 @@
 import Lidar_Detection_Thread
 import prise_de_decision
-#import interface
+import interface
 import Ultrason
 import time
 import can
@@ -8,40 +8,40 @@ import sys
 import os
 import struct
 
+global DATAINTERFACE
+global DATALIDAR
+global DATAULTRASONIC
+
 if __name__ == "__main__":
 
     print('Bring up CAN0....')
     os.system("sudo /sbin/ip link set can0 up type can bitrate 400000")
     time.sleep(0.1)
 
-    try:
-        bus = can.interface.Bus(channel='can0', bustype='socketcan_native')
-    except OSError:
-        print('Cannot find PiCAN board.')
-        exit()
+    bus = can.interface.Bus(channel='can0', bustype='socketcan_native')
 
-        lidar_instance = Lidar_Detection_Thread
-        #interface_instance = interface
-        ultrason_instance = Ultrason
-        #decision_instance = prise_de_decision
+    lidar_instance = Lidar_Detection_Thread
+    interface_instance = interface
+    ultrason_instance = Ultrason
+    #decision_instance = prise_de_decision
 
-        lidar_thread = lidar_instance.LidarDetection()
-        #interface_thread = interface_instance.Interface()
-        ultrason_thread = ultrason_instance.Ultrason(bus)
-        #decision_thread = decision_instance
+    lidar_thread = lidar_instance.LidarDetection()
+    interface_thread = interface_instance.Interface()
+    ultrason_thread = ultrason_instance.Ultrason(bus)
+    #decision_thread = decision_instance
 
-        lidar_thread.daemon = True
-        #interface_thread.daemon = True
-        ultrason_thread.daemon = True
-        #decision_thread.daemon = True
+    lidar_thread.daemon = True
+    interface_thread.daemon = True
+    ultrason_thread.daemon = True
+    #decision_thread.daemon = True
 
-        lidar_thread.start()
-        #interface_thread.start()
-        ultrason_thread.start()
-        #decision_thread.start()
+    lidar_thread.start()
+    interface_thread.start()
+    ultrason_thread.start()
+    #decision_thread.start()
 
-        lidar_thread.join()
-        # interface_thread._stop.clear()
-        ultrason_thread.join()
-        # decision_thread.join()
+    lidar_thread.join()
+    interface_thread.join()
+    ultrason_thread.join()
+    # decision_thread.join()
 
