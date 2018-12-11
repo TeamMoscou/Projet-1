70a8227c498873c7e299e0d8ffb005e261df65d6
kuh0729 <41353984+kuh0729@users.noreply.github.com>
2018-12-11 08:36:57 +0100

    Add files via upload

--- raspberry/server/server.py
+++ raspberry/server/server.py
@@ -20,28 +20,28 @@ if __name__ == "__main__":
         print('Cannot find PiCAN board.')
         exit()
 
-    lidar_instance = Lidar_Detection_Thread
-    #interface_instance = interface
-    ultrason_instance = Ultrason
-    #decision_instance = prise_de_decision
+        lidar_instance = Lidar_Detection_Thread
+        #interface_instance = interface
+        ultrason_instance = Ultrason
+        #decision_instance = prise_de_decision
 
-    lidar_thread = lidar_instance.LidarDetection()
-    #interface_thread = interface_instance.Interface()
-    ultrason_thread = ultrason_instance.Ultrason(bus)
-    #decision_thread = decision_instance
+        lidar_thread = lidar_instance.LidarDetection()
+        #interface_thread = interface_instance.Interface()
+        ultrason_thread = ultrason_instance.Ultrason(bus)
+        #decision_thread = decision_instance
 
-    lidar_thread.daemon = True
-    #interface_thread.daemon = True
-    ultrason_thread.daemon = True
-    #decision_thread.daemon = True
+        lidar_thread.daemon = True
+        #interface_thread.daemon = True
+        ultrason_thread.daemon = True
+        #decision_thread.daemon = True
 
-    lidar_thread.start()
-    #interface_thread.start()
-    ultrason_thread.start()
-    #decision_thread.start()
+        lidar_thread.start()
+        #interface_thread.start()
+        ultrason_thread.start()
+        #decision_thread.start()
 
-    lidar_thread.join()
-    # interface_thread._stop.clear()
-    ultrason_thread.join()
-    # decision_thread.join()
+        lidar_thread.join()
+        # interface_thread._stop.clear()
+        ultrason_thread.join()
+        # decision_thread.join()
 
