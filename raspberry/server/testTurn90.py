e3b89c1cb79cade74f48d85d8404a2b5ef07f843
M. Ojeda Pineda Andres Leonardo <ojedapin@insa-07444>
2018-12-19 17:15:51 +0100

    avec capteurs IMU

--- raspberry/server/testTurn90.py
+++ raspberry/server/testTurn90.py
@@ -3,6 +3,13 @@ import can
 import os
 import struct
 
+OM1 = 0x101
+
+toutDroit = can.Message(arbitration_id=0x010,data=[0xbc,0xbc,0x00, 0x00, 0x00, 0x00,0x00, 0x00],extended_id=False)
+tournerDroit = can.Message(arbitration_id=0x010,data=[0xc4,0xa0,0x00, 0x00, 0x00, 0x00,0x00, 0x00],extended_id=False)
+tournerGauche = can.Message(arbitration_id=0x010,data=[0xa0,0xc4,0x00, 0x00, 0x00, 0x00,0x00, 0x00],extended_id=False)
+arret = can.Message(arbitration_id=0x010,data=[0x00,0x00,0x00, 0x00, 0x00, 0x00,0x00, 0x00],extended_id=False)
+
 print('Bring up CAN0....')
 os.system("sudo ifconfig can0 down")
 os.system("sudo /sbin/ip link set can0 up type can bitrate 400000")
@@ -18,15 +25,29 @@ except OSError:
 
 # Main loop
 try:
-    msg = can.Message(arbitration_id=0x010,data=[0xc2,0xa2,0x00, 0x00, 0x00, 0x00,0x00, 0x00],extended_id=False)
-    bus.send(msg)
-    time.sleep(20)
-    msg = can.Message(arbitration_id=0x010,data=[0x00,0x00,0x00, 0x00, 0x00, 0x00,0x00, 0x00],extended_id=False)
-    bus.send(msg)
+    #msg = can.Message(arbitration_id=0x010,data=[0xc4,0xa0,0x00, 0x00, 0x00, 0x00,0x00, 0x00],extended_id=False)
+    #bus.send(msg)
+    #time.sleep(10.5)
+    #msg = can.Message(arbitration_id=0x010,data=[0x00,0x00,0x00, 0x00, 0x00, 0x00,0x00, 0x00],extended_id=False)
+    while True:
+        msg = bus.recv()
+        if msg.arbitration_id == OM1:
+            yaw = struct.unpack('>f',msg.data[0:4])
+            angle = int(yaw[0])
+            print("angle 0 : " + str(angle))
+            break
+    bus.send(tournerDroit)
+    while True:
+        msg = bus.recv()
+        if msg.arbitration_id == OM1:
+            yaw = struct.unpack('>f',msg.data[0:4])
+            print("angle : " + str(int(yaw[0])))
+            if abs(int(yaw[0])) > angle+90 and  abs(int(yaw[0])) < angle+90 : break 
+    bus.send(arret)      
 
 except KeyboardInterrupt:
 	#Catch keyboard interrupt
-    msg = can.Message(arbitration_id=0x010,data=[0x00,0x00,0x00, 0x00, 0x00, 0x00,0x00, 0x00],extended_id=False)
-    bus.send(msg)
+    #msg = can.Message(arbitration_id=0x010,data=[0x00,0x00,0x00, 0x00, 0x00, 0x00,0x00, 0x00],extended_id=False)
+    bus.send(arret)
     os.system("sudo /sbin/ip link set can0 down")
 print('\n\rKeyboard interrtupt')
