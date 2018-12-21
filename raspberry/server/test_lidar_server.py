7347ebea052ed2e3756ce3848c3dc282a63be524
Dreetai <jongrab@gmail.com>
2018-12-21 15:54:45 +0100

    testlidar

--- raspberry/server/test_lidar_server.py
+++ raspberry/server/test_lidar_server.py
@@ -1,3 +1,17 @@
+import test_lidar_detection_thread
+import lidar_detection_thread
+import prise_decision
+import interface
+import ultrason
+import can_send
+import time
+import can
+import sys
+import os
+import struct
+import data
+import socket
+import signal
 
 if __name__ == "__main__":
 
