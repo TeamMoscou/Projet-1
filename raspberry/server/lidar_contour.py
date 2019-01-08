91106cf4dd3abbeb04f3919ec49e482e331e70af
Dreetai <jongrab@gmail.com>
2019-01-08 17:27:46 +0100

    lidar

--- raspberry/server/lidar_contour.py
+++ raspberry/server/lidar_contour.py
@@ -14,9 +14,9 @@ from data import *
 #lidar = RPLidar('/dev/ttyUSB0')
 class LidarDetection(threading.Thread):
 
-    def __init__(self):
+    def __init__(self,lidar):
         threading.Thread.__init__(self)
-        self.lidar = RPLidar('/dev/ttyUSB0')
+        self.lidar = lidar
 
     def run(self):
 
