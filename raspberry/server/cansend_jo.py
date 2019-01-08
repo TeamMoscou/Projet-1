86942f2e86b97fe3b25a162fbc2e659e0d604c80
Dreetai <jongrab@gmail.com>
2019-01-08 17:37:43 +0100

    bon cansend jo

--- raspberry/server/cansend_jo.py
+++ raspberry/server/cansend_jo.py
@@ -1,88 +1,86 @@
 # coding: utf-8
-from threading import Thread
-class Can_send(Thread):
+import threading
+import can
+import time
+import glob
+from glob import *
+
+class Can_send(threading.Thread):
     def __init__(self, bus):
-        Thread.__init__(self)
-        #print('Bring up CAN0....')
-        #os.system("sudo ifconfig can0 down")
-        #os.system("sudo /sbin/ip link set can0 up type can bitrate 400000")
-        #time.sleep(0.1)
-        #try:
+        threading.Thread.__init__(self)
         self.bus = bus
-        #except OSError:
-         #   print('Cannot find PiCAN board.')
-          #  exit()
-      
+        glob.DATA_DECISION = Data(ID.DECISION, Message.STOP)  
+        #global test_dec
+
     def run(self):
         self.speed_cmd = 0
         self.move = 0
         self.turn = 0
         self.enable = 0
-        while True :
-            self.speed_cmd = 60
-            print("speed is fixed to ", self.speed_cmd)
-            if (DATA_FINAL == FORWARD):
+        while True:
+            time.sleep(0.1)
+            self.speed_cmd = 25
+            if (glob.DATA_DECISION.message == Message.FORWARD):
                 self.move = 1
                 self.turn = 0
                 self.enable = 1
                 print("send cmd move forward")
-            elif (DATA_FINAL == FORWARD_LEFT):
+            elif (glob.DATA_DECISION.message == Message.FORWARD_LEFT):
                 self.move = 1
                 self.turn = -1
                 self.enable = 1
                 print("send cmd move forward_left")
-            elif (DATA_FINAL == FORWARD_RIGHT):
+            elif (glob.DATA_DECISION.message == Message.FORWARD_RIGHT):
                 self.move = 1
                 self.turn = 1
                 self.enable = 1
                 print("send cmd move forward_right")
-            elif (DATA_FINAL == BACKWARD):
+            elif (glob.DATA_DECISION.message == Message.BACKWARD):
                 self.move = -1
                 self.turn = 0
                 self.enable = 1
                 print("send cmd move backward")
-            elif (DATA_FINAL == BACKWARD_LEFT):
+            elif (glob.DATA_DECISION.message == Message.BACKWARD_LEFT):
                 self.move = -1
                 self.turn = -1
                 self.enable = 1
                 print("send cmd move backward_left")
-            elif (DATA_FINAL == BACKWARD_RIGHT):
+            elif (glob.DATA_DECISION.message == Message.BACKWARD_RIGHT):
                 self.move = -1
                 self.turn = 1
                 self.enable = 1
                 print("send cmd move backward_right")
-            elif (DATA_FINAL == LEFT):
+            elif (glob.DATA_DECISION.message == Message.LEFT):
                 self.move = 0
                 self.turn = -1
                 self.enable = 1
                 print("send cmd turn left")
-            elif (DATA_FINAL == RIGHT):
+            elif (glob.DATA_DECISION.message == Message.RIGHT):
                 self.move = 0
                 self.turn = 1
                 self.enable = 1
                 print("send cmd turn right")
-            elif (DATA_FINAL == STOP_STEERING):
-                self.turn = 0
-                self.enable = 0
-                print("send cmd stop to turn")
-            elif (DATA_FINAL == STOP):
+            elif (glob.DATA_DECISION.message == Message.STOP):
                 self.move = 0
                 self.turn = 0
                 self.enable = 0
                 print("send cmd move stop")
 
-            print("Move: ",self.move)
-            print("Turn: ",self.turn)
-            print("Enable: ",self.enable)
+ #print("Data decision: ", DATA_DECISION.message)
+
             if self.enable:
-                cmd_mv = (50 + self.move*self.speed_cmd) | 0x80
-                cmd_turn = 50 + self.turn*20 | 0x80
+                cmd_mv = (50 + self.move * self.speed_cmd) | 0x80
+                cmd_turn = 50 + self.turn * 20 | 0x80
             else:
-                cmd_mv = (50 + self.move*self.speed_cmd) & ~0x80
-                cmd_turn = 50 + self.turn*20 & 0x80
-            print("mv:",cmd_mv,"turn:",cmd_turn)
-            msg = can.Message(arbitration_id=MCM,data=[cmd_mv, cmd_mv, cmd_turn,0,0,0,0,0],extended_id=False)
-            #msg = can.Message(arbitration_id=0x010,data=[0xBC,0xBC,0x00, 0x00, 0x00, 0x00,0x00, 0x00],extended_id=False)
-            #msg = can.Message(arbitration_id=MCM,data=[0xBC,0xBC,0x00, 0x00, 0x00, 0x00,0x00, 0x00],extended_id=False)
-            print(msg)
+                cmd_mv = 0x00
+                cmd_turn = 0x00
+                #cmd_mv = (50 + self.move * self.speed_cmd) & ~0x80
+                #cmd_turn = 50 + self.turn * 20 & 0x80
+
+#            print("mv:", cmd_mv, "turn:", cmd_turn)
+            msg = can.Message(arbitration_id=0x010, data=[cmd_mv, cmd_mv, cmd_turn, 0x00, 0x00, 0x00, 0x00, 0x00], extended_id=False)
+              
+            #msg = can.Message(arbitration_id=0x010,data=[0x00,0x00,0x00, 0x00, 0x00, 0x00,0x00, 0x00],extended_id=False)
+            # msg = can.Message(arbitration_id=MCM,data=[0xBC,0xBC,0x00, 0x00, 0x00, 0x00,0x00, 0x00],extended_id=False)
+            #print(msg)
             self.bus.send(msg)
