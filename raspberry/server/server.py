223eb424085771c4fa3ef9d919fa891395ec4bf6
Dreetai <jongrab@gmail.com>
2019-01-09 17:52:46 +0100

    tad

--- raspberry/server/server.py
+++ raspberry/server/server.py
@@ -24,17 +24,17 @@ def signal_handler(sig, frame):
   conn.close()
 
 
-'''
-HOST = ''
-PORT = 6666
-lidar=RPLidar('/dev/ttyUSB0')
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(1)
-conn, addr = s.accept()
-print('Connected by', addr)
-'''
+#HOST = ''
+#PORT = 6666
+#lidar=RPLidar('/dev/ttyUSB0')
+
+#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#s.bind((HOST, PORT))
+#s.listen(1)
+#conn, addr = s.accept()
+#print('Connected by', addr)
+
 if __name__ == "__main__":
   try:
     print('Bring up CAN0....')
@@ -46,52 +46,52 @@ if __name__ == "__main__":
     
     
     
-    '''lidar_instance = lidar_contour
+    #lidar_instance = lidar_contour
     #lidar_instance = lidar_detection_thread
     #interface_instance = interface
     #interfaceReturn_instance = interface
     #ultrason_instance = ultrason
-    decision_instance = prise_decision
+    #decision_instance = prise_decision
     #cansend_instance = can_send'''
     cansend_instance = cansend_jo
 
-    '''lidar_thread = lidar_instance.LidarDetection(lidar)
+    #lidar_thread = lidar_instance.LidarDetection(lidar)
     #interface_thread = interface_instance.Interface(conn)
     #interfaceReturn_thread = interface_instance.ReturnInterface(conn)
     #ultrason_thread = ultrason_instance.Ultrason(bus)
-    decision_thread = decision_instance.Prise_decision()'''
+    #decision_thread = decision_instance.Prise_decision()
     cansend_thread = cansend_instance.Can_send(bus)
 
 
-    '''lidar_thread.daemon = True
+    #lidar_thread.daemon = True
     #interface_thread.daemon = True
     #interfaceReturn_thread.daemon = True
     #ultrason_thread.daemon = True
-    decision_thread.daemon = True'''
+    #decision_thread.daemon = True
     cansend_thread.daemon = True
     
     
-   ''' signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGINT, signal_handler)
 
     
     #interface_thread.start()
     #interfaceReturn_thread.start()
-    lidar_thread.start()
+    #lidar_thread.start()
     #ultrason_thread.start()
-    decision_thread.start()'''
+    #decision_thread.start()
     cansend_thread.start()
 
-    '''lidar_thread.join()
+    #lidar_thread.join()
     #interface_thread.join()
     #interfaceReturn_thread.join()
     #ultrason_thread.join()
-    decision_thread.join()'''
+    #decision_thread.join()
     cansend_thread.join()
-'''
+
   except:
     print('an exception raised!')
-    lidar.stop()
-    lidar.stop_motor()
-    lidar.disconnect()
-    conn.close()'''
+    #lidar.stop()
+    #lidar.stop_motor()
+    #lidar.disconnect()
+    #conn.close()
     
