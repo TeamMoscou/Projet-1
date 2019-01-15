5414c40b7f4a5cb2aa80217581a75ab30193dbcb
TeamMoscou_INSA_TLSE_GEI <43847983+TeamMoscou@users.noreply.github.com>
2019-01-15 10:48:21 +0100

    Update ultrason.py

--- raspberry/server/ultrason.py
+++ raspberry/server/ultrason.py
@@ -22,12 +22,12 @@ class Ultrason(threading.Thread):
         flagUltrasonArriereGauche = 0
         flagUltrasonArriereCentre = 0
         
-        #Nombre de valeur consecutives voulues avant de considerer OK
+        #Number of consecutive values wanted before considering obstale is detection
         NbVal = 5 
         
-        #Membre : 0 :  AvantGauche, 1 : AvantDroite, 2 : Avant centre, 3 : Arriere Gauche, 4 : Arriere Droit, 5 : Arriere Centre 
+        #Membres : 0 :  ForwardLeft, 1 : ForwardRight, 2 : ForwardCentre, 3 : BackwardLeft, 4 : BackwardRight, 5 : BackwardCentre 
         compteur=[0,0,0,0,0,0]
-        #Verifie si au message precedent c'etait la meme valeur ou pas : Si 1 : Il y avait qq chose au tour precedent, si 0 rien. 
+        #Check if the previous message was the same value or not : If 1: There was something in the previous round, if 0 nothing. 
         compteurON=[0,0,0,0,0,0] 
         
         while True:
@@ -35,16 +35,16 @@ class Ultrason(threading.Thread):
             
             if msg.arbitration_id == US1:
                 
-                #Ultrason AvantGauche
+                #Ultrasonic ForwardLeft
                 distance = int.from_bytes(msg.data[0:2], byteorder='big')
                 if distance <= 20:
-                    #On verifie si on voyait la meme chose avant (obstacle ici). Si c'est le cas on incremente le compteur. 
+                    #We check if we saw the same thing before (obstacle here). If so, increment the counter. 
                     if compteurON[0] == 1 :
                         compteur[0] = compteur[0] + 1 
                     else : 
                         compteurON[0] = 1
                         compteur[0] = 1
-                    #Une fois arrive a un certain nombre de valeurs consecutives ou plus, on considere qu'un obstacle est present 
+                    #Once the number of consecutive values  exceeds NBVal an obstacle is considered
                     if compteur[0] >= NbVal :
                         flagUltrasonAvantGauche=1
                         
@@ -54,19 +54,20 @@ class Ultrason(threading.Thread):
                     else : 
                         compteurON[0] = 0
                         compteur[0] = 1
-                    #Une fois arrive a un certain nombre de valeurs consecutives ou plus, on considere qu'un obstacle est present 
+                    #Once the number of consecutive values  exceeds NBVal an obstacle is considered
                     if compteur[0] >= NbVal :
                         flagUltrasonAvantGauche=0     
                     
-                #Ultrason AvantDroite
+                #Ultrasonic ForwardRight
                 distance = int.from_bytes(msg.data[2:4],byteorder='big')
                 if distance <= 20:
+                    #We check if we saw the same thing before (obstacle here). If so, increment the counter. 
                     if compteurON[1] == 1 :
                         compteur[1] = compteur[1] + 1 
                     else : 
                         compteurON[1] = 1
                         compteur[1] = 1
-                    #Une fois arrive a un certain nombre de valeurs consecutives ou plus, on considere qu'un obstacle est present 
+                    #Once the number of consecutive values  exceeds NBVal an obstacle is considered
                     if compteur[1] >= NbVal :
                         flagUltrasonAvantDroit=1
                         print("Avant droit detected")
@@ -76,19 +77,20 @@ class Ultrason(threading.Thread):
                     else : 
                         compteurON[1] = 0
                         compteur[1] = 1
-                    #Une fois arrive a un certain nombre de valeurs consecutives ou plus, on considere qu'un obstacle est present 
+                    #Once the number of consecutive values  exceeds NBVal an obstacle is considered
                     if compteur[1] >= NbVal :
                         flagUltrasonAvantDroit=0  
                         
-                #Ultrason ArriereCentre 
+                #Ultrasonic BackwardCentre 
                 distance = int.from_bytes(msg.data[4:6], byteorder='big')
                 if distance <= 50:
+                    #We check if we saw the same thing before (obstacle here). If so, increment the counter. 
                     if compteurON[5] == 1 :
                         compteur[5] = compteur[5] + 1 
                     else : 
                         compteurON[5] = 1
                         compteur[5] = 1
-                    #Une fois arrive a un certain nombre de valeurs consecutives ou plus, on considere qu'un obstacle est present 
+                    #Once the number of consecutive values  exceeds NBVal an obstacle is considered
                     if compteur[5] >= NbVal :                
                         flagUltrasonArriereCentre=1
                         
@@ -98,22 +100,23 @@ class Ultrason(threading.Thread):
                     else : 
                         compteurON[5] = 0
                         compteur[5] = 1
-                    #Une fois arrive a un certain nombre de valeurs consecutives ou plus, on considere qu'un obstacle est present 
+                    #Once the number of consecutive values  exceeds NBVal an obstacle is considered
                     if compteur[5] >= NbVal :
                         flagUltrasonArriereCentre=0   
                         
                         
             elif msg.arbitration_id == US2:
                 
-                # ultrason arriere gauche
+                #Ultrasonic BackwardLeft
                 distance = int.from_bytes(msg.data[0:2], byteorder='big')
                 if distance <= 20:
+                    #We check if we saw the same thing before (obstacle here). If so, increment the counter. 
                     if compteurON[3] == 1 :
                         compteur[3] = compteur[3] + 1 
                     else : 
                         compteurON[3] = 1
                         compteur[3] = 1
-                    #Une fois arrive a un certain nombre de valeurs consecutives ou plus, on considere qu'un obstacle est present 
+                    #Once the number of consecutive values  exceeds NBVal an obstacle is considered
                     if compteur[3] >= NbVal :
                         flagUltrasonArriereGauche=1
                         print("Arriere gauche detected")
@@ -123,19 +126,20 @@ class Ultrason(threading.Thread):
                     else : 
                         compteurON[3] = 0
                         compteur[3] = 1
-                    #Une fois arrive a un certain nombre de valeurs consecutives ou plus, on considere qu'un obstacle est present 
+                    #Once the number of consecutive values  exceeds NBVal an obstacle is considered
                     if compteur[3] >= NbVal :
                         flagUltrasonArriereGauche=0 
                         
-                # ultrason arriere droit
+                #Ultrasonic BackwardRight
                 distance = int.from_bytes(msg.data[2:4], byteorder='big')
                 if distance <= 20:
+                    #We check if we saw the same thing before (obstacle here). If so, increment the counter. 
                     if compteurON[4] == 1 :
                         compteur[4] = compteur[4] + 1 
                     else : 
                         compteurON[4] = 1
                         compteur[4] = 1
-                    #Une fois arrive a un certain nombre de valeurs consecutives ou plus, on considere qu'un obstacle est present 
+                    #Once the number of consecutive values  exceeds NBVal an obstacle is considered
                     if compteur[4] >= NbVal :
                         flagUltrasonArriereDroit=1
                         print("Arriere droit detected")
@@ -145,19 +149,20 @@ class Ultrason(threading.Thread):
                     else : 
                         compteurON[4] = 0
                         compteur[4] = 1
-                    #Une fois arrive a un certain nombre de valeurs consecutives ou plus, on considere qu'un obstacle est present 
+                    #Once the number of consecutive values  exceeds NBVal an obstacle is considered
                     if compteur[4] >= NbVal :
                         flagUltrasonArriereDroit=0   
                         
-                # ultrason avant centre
+                #Ultrasonic ForwardCentre
                 distance = int.from_bytes(msg.data[4:6], byteorder='big')
                 if distance <= 50:
+                    #We check if we saw the same thing before (obstacle here). If so, increment the counter. 
                     if compteurON[2] == 1 :
                         compteur[2] = compteur[2] + 1 
                     else : 
                         compteurON[2] = 1
                         compteur[2] = 1
-                    #Une fois arrive a un certain nombre de valeurs consecutives ou plus, on considere qu'un obstacle est present 
+                    #Once the number of consecutive values  exceeds NBVal an obstacle is considered
                     if compteur[2] >= NbVal :
                         flagUltrasonAvantCentre=1
                         
@@ -167,7 +172,7 @@ class Ultrason(threading.Thread):
                     else : 
                         compteurON[2] = 0
                         compteur[2] = 1
-                    #Une fois arrive a un certain nombre de valeurs consecutives ou plus, on considere qu'un obstacle est present 
+                    #Once the number of consecutive values  exceeds NBVal an obstacle is considered
                     if compteur[2] >= NbVal :
                         flagUltrasonAvantCentre=0     
                         
