b740fe1986adba6743efc6727131275c2d54708d
M. Vignolles Amaury <avignoll@insa-08135>
2019-01-18 09:22:48 +0100

    modif reset variables

--- raspberry/server/prise_decision.py
+++ raspberry/server/prise_decision.py
@@ -7,6 +7,12 @@ class Prise_decision(threading.Thread):
 
     def __init__(self):
         threading.Thread.__init__(self)
+        #Init des variables locales
+        Stop_requested = 0
+        Detection_front = 0
+        Detection_back = 0 
+        Forward = 0
+        Backward = 0
 
     def run(self):
         #To be sure that each module has started
@@ -16,32 +22,35 @@ class Prise_decision(threading.Thread):
             #We consider checking variables 10 times per second is enough
             time.sleep(0.1)
 
-            #Reset test flags
-            Stop_requested = 0
-            Detection_front = 0
-            Detection_back = 0
-            Forward = 0
-            Backward = 0
-
             #Reading global variables and change the state of them
             if (DATA_INTERFACE.message == Message.STOP):
                 Stop_requested = 1
+            else:
+                Stop_requested = 0    
 
             #Ultrasonic or Lidar detect something in front of the car
             if (DATA_ULTRASONIC.message == Message.DETECTED_FRONT or DATA_LIDAR.message == Message.DETECTED_FRONT or DATA_LIDAR.message == Message.DETECTED_BOTH or DATA_ULTRASONIC.message == Message.DETECTED_BOTH):
                 Detection_front = 1
+            else:
+                Detection_front = 0
 
             #Ultrasonic or Lidar detect something at the back of the car
             if (DATA_ULTRASONIC.message == Message.DETECTED_BACK or DATA_LIDAR.message == Message.DETECTED_BACK or DATA_LIDAR.message == Message.DETECTED_BOTH or DATA_ULTRASONIC.message == Message.DETECTED_BOTH):
                 Detection_back = 1
+            else: 
+                Detection_back = 0    
 
             #Command forward threw the interface or the autonomous mode
             if (DATA_INTERFACE.message == Message.FORWARD or DATA_INTERFACE.message == Message.FORWARD_RIGHT or DATA_INTERFACE.message == Message.FORWARD_LEFT or MODE == "AUTONOMOUS"):
                 Forward = 1
+            else:
+                Forward = 0    
 
             #Command backward threw the interface
             if (DATA_INTERFACE.message == Message.BACKWARD or DATA_INTERFACE.message == Message.BACKWARD_RIGHT or DATA_INTERFACE.message == Message.BACKWARD_LEFT):
                 Backward = 1
+            else:
+                Backward = 0    
             
             print(" Detection_front ", Detection_front, " Forward ",Forward, " Detection_back ",Detection_back," Backward ",Backward)
             #Generating decision message
