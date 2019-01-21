6310903e1b67a679d24a4a9e05ef57d83a73e64f
TeamMoscou_INSA_TLSE_GEI <43847983+TeamMoscou@users.noreply.github.com>
2019-01-21 09:56:44 +0100

    Update main.py

--- abdelilah/main.py
+++ abdelilah/main.py
@@ -51,7 +51,7 @@ if __name__ == "__main__":
     #instanciate Threads
     lidar_thread = lidar_instance.LidarDetection(lidar)
     interface_thread = interface_instance.Interface(conn)
-    #interfaceReturn_thread = interface_instance.ReturnInterface(conn)
+    interfaceReturn_thread = interface_instance.ReturnInterface(conn)
     #ultrason_thread = ultrason_instance.Ultrason(bus)
     decision_thread = decision_instance.Prise_decision()
     cansend_thread = cansend_instance.Can_send(bus)
@@ -59,7 +59,7 @@ if __name__ == "__main__":
     #set the Threads as daemon
     lidar_thread.daemon = True
     interface_thread.daemon = True
-    #interfaceReturn_thread.daemon = True
+    interfaceReturn_thread.daemon = True
     #ultrason_thread.daemon = True
     decision_thread.daemon = True
     cansend_thread.daemon = True
@@ -67,7 +67,7 @@ if __name__ == "__main__":
     #start the Threads
     lidar_thread.start()
     interface_thread.start()
-    #interfaceReturn_thread.start()
+    interfaceReturn_thread.start()
     #ultrason_thread.start()
     decision_thread.start()
     cansend_thread.start()
@@ -75,7 +75,7 @@ if __name__ == "__main__":
     #wait until the Threads finish
     lidar_thread.join()
     interface_thread.join()
-    #interfaceReturn_thread.join()
+    interfaceReturn_thread.join()
     #ultrason_thread.join()
     decision_thread.join()
     cansend_thread.join()
